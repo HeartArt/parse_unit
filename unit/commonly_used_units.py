5ba18824d348f36baaa27d70bd44c5603151b2ad
George Wang <49050519+HeartArt@users.noreply.github.com>
2026-08-17 16:50:44 -0700

    Add files via upload

---
+++ unit/commonly_used_units.py
@@ -0,0 +1,41 @@
+from unit.parser import create_unit_from_string
+
+# angles
+hr_angle = create_unit_from_string('hourangle')
+deg = create_unit_from_string('degree')
+arcsec = create_unit_from_string('arcsec')
+steradian = create_unit_from_string('steradian')
+
+# distance
+Mpc = create_unit_from_string('Mpc')
+kpc = create_unit_from_string('kpc')
+pc = create_unit_from_string('pc')
+m = create_unit_from_string('m')
+mm = create_unit_from_string('mm')
+um = create_unit_from_string('micron')
+
+# frequency
+GHz = create_unit_from_string('GHz')
+Hz = create_unit_from_string('Hz')
+
+# speed
+kms = create_unit_from_string('km/s')
+ms = create_unit_from_string('m/s')
+
+# brightness
+MJy = create_unit_from_string('MJy')
+Jy = create_unit_from_string('Jy')
+mJy = create_unit_from_string('mJy')
+uJy = create_unit_from_string('uJy')
+solLum = create_unit_from_string('solLum')
+
+# temperature
+K = create_unit_from_string('K')
+
+# mass
+solMass = create_unit_from_string('solMass')
+kg = create_unit_from_string('kg')
+
+# time
+yr = create_unit_from_string('yr')
+s = create_unit_from_string('s')
