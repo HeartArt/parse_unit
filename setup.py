86916bf6db12b2c273210d58910c32d1a1622f03
George Wang <49050519+HeartArt@users.noreply.github.com>
2026-08-17 16:42:39 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="parse_unit",           # How the package is named in pip (e.g., pip install your_package_name)
+    version="0.1.0",                    # Your current release version
+    description="Parse and store commonly used units", 
+    author="GW",
+    packages=find_packages(),           # Automatically finds folders with an __init__.py
+    install_requires=[                  # Dependencies pip will automatically install
+        "numpy",
+        "astropy"
+    ],
+    python_requires=">=3.8",            # Minimum Python version required
+)
