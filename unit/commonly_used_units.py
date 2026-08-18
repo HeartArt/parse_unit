964ecc16a8be8873ab8581aa90f5d4516aa00ce7
George Wang <49050519+HeartArt@users.noreply.github.com>
2026-08-17 17:07:25 -0700

    Update commonly_used_units.py

--- unit/commonly_used_units.py
+++ unit/commonly_used_units.py
@@ -1,41 +1,65 @@
 from unit.parser import create_unit_from_string
 
 # angles
-hr_angle = create_unit_from_string('hourangle')
-deg = create_unit_from_string('degree')
-arcsec = create_unit_from_string('arcsec')
-steradian = create_unit_from_string('steradian')
+def hr_angle():
+  return create_unit_from_string('hourangle')
+def deg():
+  return create_unit_from_string('degree')
+def arcsec():
+  return create_unit_from_string('arcsec')
+def steradian():
+  return create_unit_from_string('steradian')
 
 # distance
-Mpc = create_unit_from_string('Mpc')
-kpc = create_unit_from_string('kpc')
-pc = create_unit_from_string('pc')
-m = create_unit_from_string('m')
-mm = create_unit_from_string('mm')
-um = create_unit_from_string('micron')
+def Mpc():
+  return create_unit_from_string('Mpc')
+def kpc():
+  return create_unit_from_string('kpc')
+def pc():
+  return create_unit_from_string('pc')
+def m():
+  return create_unit_from_string('m')
+def mm():
+  return create_unit_from_string('mm')
+def um():
+  return create_unit_from_string('micron')
 
 # frequency
-GHz = create_unit_from_string('GHz')
-Hz = create_unit_from_string('Hz')
+def GHz():
+  return reate_unit_from_string('GHz')
+def Hz():
+  return create_unit_from_string('Hz')
 
 # speed
-kms = create_unit_from_string('km/s')
-ms = create_unit_from_string('m/s')
+def kms():
+  return create_unit_from_string('km/s')
+def ms():
+  return create_unit_from_string('m/s')
 
 # brightness
-MJy = create_unit_from_string('MJy')
-Jy = create_unit_from_string('Jy')
-mJy = create_unit_from_string('mJy')
-uJy = create_unit_from_string('uJy')
-solLum = create_unit_from_string('solLum')
+def MJy():
+  return create_unit_from_string('MJy')
+def Jy():
+  return create_unit_from_string('Jy')
+def mJy():
+  return create_unit_from_string('mJy')
+def uJy():
+  return create_unit_from_string('uJy')
+def solLum():
+  return create_unit_from_string('solLum')
 
 # temperature
-K = create_unit_from_string('K')
+def K():
+  return create_unit_from_string('K')
 
 # mass
-solMass = create_unit_from_string('solMass')
-kg = create_unit_from_string('kg')
+def solMass():
+  return create_unit_from_string('solMass')
+def kg():
+  return create_unit_from_string('kg')
 
 # time
-yr = create_unit_from_string('yr')
-s = create_unit_from_string('s')
+def yr():
+  return create_unit_from_string('yr')
+def s():
+  return create_unit_from_string('s')
